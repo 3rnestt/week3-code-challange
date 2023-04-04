abbcf9a2038903e7e604bd16bd4292e64e3dbf53
Ernest <ernest.wambugu@student.moringaschool.com>
2023-04-04 15:18:23 +0300

    Uploaded the db.json file and eidted the style sheet for the website

--- app.js
+++ app.js
@@ -1,74 +1,89 @@
-// 1. Fetch movie data
-fetch("db.json")
-  .then(response => response.json())
-  .then(data => {
-    // 2. Extract details of the first movie
-    const movie = data.films[0];
-    const { poster, title, runtime, showtime, capacity, tickets_sold } = movie;
-    availableTickets = capacity - tickets_sold;
+ //Function to display first movie details
+ document.addEventListener("DOMContentLoaded",()=>{
 
-    // 3. Set details in the HTML elements
-    document.getElementById('poster').src = poster;
-    document.getElementById('title').textContent = title;
-    document.getElementById('runtime').textContent = `${runtime} mins`;
-    document.getElementById('showtime').textContent = showtime;
-    document.getElementById('available-tickets').textContent = availableTickets;
-
-    // 4. Create movie list
-    const filmsList = document.getElementById('films');
-    data.films.forEach(movie => {
-      const { id, title, poster, showtime } = movie;
-      const li = document.createElement('li');
-      li.innerHTML = `<img src="${poster}" alt="${title}"><div><h4>${title}</h4><p>Showtime: ${showtime}</p></div>`;
-      li.addEventListener('click', () => showMovieDetails(id));
-      filmsList.appendChild(li);
-        });
-
-    // 5. Set up buy ticket button click event taking buyTicket as a callback
-    document.getElementById('buy-ticket').addEventListener('click', buyTicket);
+    function displayMovieData(){
+       //Get request for movie data
+       fetch( 'http://localhost:3000/films')
+       .then(resp => resp.json())
+       .then(movieList => {
+          //div element creation that will contanin first movie's details
+          const movieDetails = document.createElement('div')
+          movieDetails.innerHTML = `
+          <img id='firstPoster' src ='${movieList[0].poster}'>
+          <h4>Movie title: ${movieList[0].title}</h4>
+          <h4>Runtime: ${movieList[0].runtime}</h4>
+          <h4>Showtime: ${movieList[0].showtime}</h4>
+      `
+      const movieDisplay = document.getElementById('movieDetails')
+      movieDisplay.appendChild(movieDetails)
     })
-    .catch(error => {
-    console.log(error);
-    });
-
-    // buyTicket function decreases the number of available votes by one after every
-    // click of the Buy Ticket button. The button greys out (is disabled) 
-    // and becomes ineffective once the available votes reaches 0.
-    function buyTicket() {
-      if (availableTickets > 0) {
-        availableTickets--;
-        document.getElementById('available-tickets').textContent = availableTickets;
-        if (availableTickets === 0) {
-          const buyButton = document.getElementById('buy-ticket');
-          buyButton.textContent = 'Sold Out';
-          buyButton.disabled = true;
-        }
-      }}
-   
-    // Function to update the movie details
-    function updateMovieDetails(movie) {
-      const { poster, title, runtime, showtime, capacity, tickets_sold } = movie;
-      availableTickets = capacity - tickets_sold;
-      document.getElementById('poster').src = poster;
-      document.getElementById('title').textContent = title;
-      document.getElementById('runtime').textContent = `${runtime} mins`;
-      document.getElementById('showtime').textContent = showtime;
-      document.getElementById('available-tickets').textContent = availableTickets;
-    }
-    // Function to show movie details when a movie is clicked
-    function showMovieDetails(id) {
-      fetch("db.json")
-        .then(response => response.json())
-        .then(data => {
-          const movie = data.films.find(movie => movie.id === id);
-          if (movie) {
-            updateMovieDetails(movie);
-          }
+ }
+ displayMovieData();
+ 
+ //function to display list of other movie names
+ function displayMovieListNames(){
+    //Get request for movie data
+    fetch( 'http://localhost:3000/films')
+    .then(resp => resp.json())
+    .then(movieList =>{
+       //Iteration that displays list of movie names
+       movieList.forEach(movie =>{
+          const movieItem = document.createElement('li')
+          movieItem.innerHTML = `
+          ${movie.title}
+        `
+        const movieListNames = document.getElementById('movieItems')
+        movieListNames.appendChild(movieItem)
+        //Event listener enabling user to click movie title to display movie details
+        movieItem.addEventListener('click', (event) => {
+           const moreDetailsList = document.querySelector('#movieDetails');
+           moreDetailsList.innerHTML = `
+           <img id='firstPoster' src ='${movie.poster}'>
+           <h4>Movie title: ${movie.title}</h4>
+           <h4>Runtime: ${movie.runtime}</h4>
+           <h4>Showtime: ${movie.showtime}</h4>
+           <h4>Available tickets:<span id='ticket'> ${movie.capacity - movie.tickets_sold}</span></h4>
+           <button id='buyTicket' type="button">Buy Ticket</button>
+           `
+           //Displays message when available tickets are zero
+           if(movie.capacity - movie.tickets_sold<=0){
+            moreDetailsList.innerHTML = `
+            <img id='firstPoster' src ='${movie.poster}'>
+            <h4>Movie title: ${movie.title}</h4>
+            <h4>Runtime: ${movie.runtime}</h4>
+            <h4>Showtime: ${movie.showtime}</h4>
+            <h4>Tickets are SOLD OUT!</h4>
+            `
+         }
+            
+           moreDetailsList.querySelector('#buyTicket').addEventListener('click', (event)=>{
+              event.preventDefault()
+              if(movie.tickets_sold === movie.capacity){
+               movieDetails.querySelector('#ticket').textContent= 'SOLD OUT!'
+              }else{
+              //Updating number of tickets sold and available tickets
+              movie.tickets_sold+=1;
+              movieDetails.querySelector('#ticket').textContent= movie.capacity - movie.tickets_sold
+              updateTicketSold(movie)
+              }
+         })
+           ;
         })
-      .catch(error => console.error(error));
+     })
+   })
 }
+displayMovieListNames();
 
 
-
-
-  
+//PATCH request that updates number of tickets sold
+ function updateTicketSold(movieObj){
+   console.log(movieObj.id)
+     fetch('http://localhost:3000/films/'+movieObj.id,{
+   method:'PATCH',
+   headers:{
+   "Content-Type" : "application/json"
+   },
+   body:JSON.stringify({tickets_sold:movieObj.tickets_sold})
+   })   
+}
+})
